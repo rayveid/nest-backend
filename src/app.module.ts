15a9fe362502be8b0c72119a21c0be68d5225e5c
Ray Veid <15350246+rayveid@users.noreply.github.com>
2023-01-22 19:48:42 +0530

    added users module
    Added users module, controller, service. Removed app service & controller.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`, // путь до файла конфигурации
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432, // порт postgres по-умолчанию
-      username: 'postgres', // пользователь по умолчанию
-      password: '1201',
-      database: 'nest-backend', // название db
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT), // порт postgres по-умолчанию
+      username: process.env.POSTGRES_USER, // пользователь по умолчанию
+      password: String(process.env.POSTGRES_PASSWORD),
+      database: process.env.POSTGRES_DB, // название db
       models: [],
       autoLoadModels: true, // автоматическое создание таблиц на основе переданных моделей
     }),
+    UsersModule,
   ],
 })
 export class AppModule {}
