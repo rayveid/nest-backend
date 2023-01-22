0b3827a49590457a3200fdeeb44e00f0ddb4542a
Ray Veid <15350246+rayveid@users.noreply.github.com>
2023-01-22 14:32:54 +0530

    deleted unnecessary packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const { off } = require('process');
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -21,5 +23,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    // 'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
   },
 };
