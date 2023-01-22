41ca7e2c5fcacceec937c8adaf709daa15bac02e
Ray Veid <15350246+rayveid@users.noreply.github.com>
2023-01-22 21:39:38 +0530

    Updated Users.Controller
    Implemented methods from users.service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // инжектим сервис для использования его методов
+  constructor(private usersService: UsersService) {}
+
+  @Post('create')
+  async create(@Body() dto: CreateUserDto) {
+    return await this.usersService.create(dto);
+  }
+
+  @Get('getAll')
+  async getAll() {
+    return await this.usersService.getAll();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './users.model';
 
-@Injectable()
-export class UsersService {}
+@Injectable() // можем инжектить этот сервис
+export class UsersService {
+  constructor(@InjectModel(User) private usersRepository: typeof User) {}
+
+  async create(dto: CreateUserDto) {
+    return await this.usersRepository.create(dto);
+  }
+
+  async getAll() {
+    return await this.usersRepository.findAll();
+  }
+}
